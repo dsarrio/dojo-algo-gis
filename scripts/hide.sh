41801ea02ef2232fc572fe7a6bfb3c289e44094c
David Sarrio <dsarrio@betomorrow.com>
2021-04-01 01:27:25 +0200

    -

--- scripts/hide.sh
+++ scripts/hide.sh
@@ -2,4 +2,5 @@
 
 echo -n Password: 
 read -s password
+echo
 find . -name "*.kt" -exec openssl enc -aes-256-cbc -pass pass:$password -a -salt -in {} -out {}.hidden \;
--- scripts/reveal.sh
+++ scripts/reveal.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo -n Password: 
 read -s password
 echo
 for i in $(find . -name '*.kt.hidden')
 do
     openssl enc -d -aes-256-cbc -pass pass:$password -a -in $i -out ${i%.*}
+    rm $i
 done
 
